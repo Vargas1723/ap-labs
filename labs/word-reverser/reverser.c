092b9fd1dcc0cc4fa6c109e9d47d85dea8d9c75d
Braulio Vargas <vargas23.bv@gmail.com>
2020-08-29 19:31:44 -0500

    Word Reverser
    Signed-off-by: Braulio Vargas <vargas23.bv@gmail.com>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define IN   1   /* inside a word */
+#define OUT  0   /* outside a word */
+#define ARRAY_LENGHT 50
 
 int main(){
-    // Place your magic here
-    return 0;
+  int c, i, tmp, state;
+  state = OUT;
+  char *word = (char *)malloc(sizeof(char)*20);
+  int count = 0;
+
+  while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\n' || c == '\t'){
+          state = OUT;
+          for (i = 0;  i < count/2; i++) {
+      	     tmp = word[i];
+      	     word[i] = word[count - i - 1];
+      	     word[count - i - 1] = tmp;
+          }
+          count = 0;
+          printf("%s ",word);
+        }
+        else if (state == OUT) {
+          state = IN;
+          word = (char *)malloc(sizeof(char)*20);
+        }
+        if(state == IN){
+          *(word+count++) = c;
+        }
+  }
+  printf("\n");
+  free(word);
+  return 0;
 }
