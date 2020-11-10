90f3382555eeecb3177e92d77218feb782f36cd3
Braulio Vargas <vargas23.bv@gmail.com>
2020-11-09 22:33:09 -0600

    Goroutines-vs-os-threads
    Signed-off-by: Braulio Vargas <vargas23.bv@gmail.com>

--- labs/goroutines-vs-os-threads/ping-pong.go
+++ labs/goroutines-vs-os-threads/ping-pong.go
@@ -2,10 +2,40 @@ package main
 
 import (
 	"fmt"
+	"time"
+  "math"
+	"os"
 )
-
 func main() {
+	channel := make(chan struct{})
+	msg := make(chan int)
 	var commsPerSecond int
-
-	fmt.Println("Communications Per Second : ", commsPerSecond)
+	timeStart := time.Now()
+	go func() {
+		msg <- 1
+		for {
+			commsPerSecond++
+			msg <- <-msg
+		}
+		channel <- struct{}{}
+	}()
+	go func() {
+		for {
+			commsPerSecond++
+			msg <- <-msg
+		}
+		channel <- struct{}{}
+	}()
+	for {
+		select {
+		case <-time.After(1 * time.Second):
+      x := float64(commsPerSecond)/time.Since(timeStart).Seconds()
+			fmt.Println("Communications Per Second : ", math.Round(x))
+		}
+		if commsPerSecond > 100000000 {
+			fmt.Println("End of Communication")
+			os.Exit(0)
+		}
+	}
+	<-channel
 }
--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func main() {
-	var maxStages, transitTime int
+var waitingChannel = make(chan struct{})
+var transitTime = time.Now()
 
-	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
-	fmt.Println("Time to transit trough the pipeline : ", transitTime)
+func pipeline(one chan struct{}, two chan struct{}, currentStages int, maxStages int) {
+	if currentStages <= maxStages {
+		fmt.Println("Maximum number of pipeline stages   : ", currentStages)
+		fmt.Println("Time to transit trough the pipeline : ", time.Since(transitTime))
+    transitTime = time.Now()
+		go pipeline(two, one, currentStages+1, maxStages)
+		two <- <-one
+	} else {
+    fmt.Println("Maximum number of stages reached")
+		waitingChannel <- struct{}{}
+	}
+}
+
+func main() {
+	var pipeOne chan (struct{})
+	var pipeTwo chan (struct{})
+	//go pipeline(pipeOne, pipeTwo, 0, 1941558)
+	go pipeline(pipeOne, pipeTwo, 0, 1000000)
+	<-waitingChannel
+
 }
