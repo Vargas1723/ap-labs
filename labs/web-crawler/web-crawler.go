b39334abf0a8d811e8ce6a20a8c1c62a1c7eae83
Braulio Vargas <vargas23.bv@gmail.com>
2020-11-10 22:00:57 -0600

    web-crawler
    Signed-off-by: Braulio Vargas <vargas23.bv@gmail.com>

--- labs/web-crawler/web-crawler.go
+++ labs/web-crawler/web-crawler.go
@@ -13,10 +13,12 @@
 package main
 
 import (
-	"fmt"
+  "fmt"
 	"log"
 	"os"
-
+	"time"
+  "bufio"
+	"flag"
 	"gopl.io/ch5/links"
 )
 
@@ -25,43 +27,83 @@ import (
 // enforce a limit of 20 concurrent requests.
 var tokens = make(chan struct{}, 20)
 
-func crawl(url string) []string {
-	fmt.Println(url)
+func crawl(link node) []node {
 	tokens <- struct{}{} // acquire a token
-	list, err := links.Extract(url)
+	list, err := links.Extract(link.url)
 	<-tokens // release the token
 
 	if err != nil {
-		log.Print(err)
+		// Disabled verbose logs because screen gets cluttered with them during tests
+		// Uncomment if you wish to see them
+		// log.Print(err)
 	}
-	return list
+	next := make([]node, 0, len(list))
+	for _, val := range list {
+		next = append(next, node{val, link.depth + 1})
+	}
+	return next
 }
 
 //!-sema
 
+type node struct {
+	url   string
+	depth int
+}
+func displayProgress() {
+	states := []string{"|", "/", "-", "\\"}
+	for i := 0; ; i = (i + 1) % len(states) {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Printf("\r\033[K\r%s", states[i])
+	}
+}
 //!+
 func main() {
-	worklist := make(chan []string)
+	if len(os.Args) < 4 {
+		log.Println("Usage: ./web-crawler -depth=DEPTH -results=FILENAME URL")
+		return
+	}
+	depth := flag.Int("depth", 0, "Web crawler depth")
+	results := flag.String("results", "results.txt", "Output file")
+	flag.Parse()
+	fmt.Printf("Web Crawler\nLink: %s\nDepth: %d\n", os.Args[3], *depth)
+	// Create output file and initialize buffered writer
+	f, err := os.Create(*results)
+	if err != nil {
+		panic(err)
+	}
+	w := bufio.NewWriter(f)
+	defer w.Flush()
+	worklist := make(chan []node)
 	var n int // number of pending sends to worklist
-
 	// Start with the command-line arguments.
 	n++
-	go func() { worklist <- os.Args[1:] }()
-
-	// Crawl the web concurrently.
-	seen := make(map[string]bool)
+	go func() {
+		worklist <- []node{node{os.Args[3], 0}}
+	}()
+	currentDepth := -1
+	visited := make(map[string]bool)
+	go displayProgress()
 	for ; n > 0; n-- {
 		list := <-worklist
 		for _, link := range list {
-			if !seen[link] {
-				seen[link] = true
+			if !visited[link.url] {
+				if link.depth > currentDepth {
+					currentDepth = link.depth
+				}
+				visited[link.url] = true
+				fmt.Fprintf(w, "%s\n", link.url)
+				if link.depth+1 > *depth {
+					continue
+				}
 				n++
-				go func(link string) {
+				go func(link node) {
 					worklist <- crawl(link)
 				}(link)
 			}
 		}
 	}
+  fmt.Printf("\r\033[K\rCompleted")
+	fmt.Printf("\nOutput file is ready and named as %s\n", *results)
 }
-
 //!-
